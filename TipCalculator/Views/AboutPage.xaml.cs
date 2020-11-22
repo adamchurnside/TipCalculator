20b4db6c8725036924e28a19bdcc0a4406d9968e
Adam Churnside <adam.churnside@gmail.com>
2020-11-22 01:28:48 -0800

    Add project files.

---
+++ TipCalculator/Views/AboutPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TipCalculator.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Bill_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        public void CalculateTip()
+        {
+            
+        }
+
+
+    }
+}
---
+++ TipCalculator/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using TipCalculator.ViewModels;
+
+namespace TipCalculator.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
